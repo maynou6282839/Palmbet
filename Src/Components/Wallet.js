4dbb47d77c3f3711b0925107641f1d44fa9977fb
maynou6282839 <benamoramin759@gmail.com>
2025-07-31 15:09:38 +0100

    Create Wallet.js

---
+++ Src/Components/Wallet.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Wallet({ balance = 1000 }) {
+  return (
+    <div className="bg-gray-900 p-4 rounded-xl shadow-md mb-4 w-80">
+      <h3 className="text-lg">Wallet Balance (TND)</h3>
+      <p className="text-yellow-400 text-xl">{balance} TND</p>
+    </div>
+  );
+}
